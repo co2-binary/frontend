e6de75311805fe5ff0b76d184781d692bc964c2b
Ilya Zlobintsev <ilya.zl@protonmail.com>
2021-09-10 14:37:55 +0300

    Home page

--- src/Home.svelte
+++ src/Home.svelte
@@ -1,4 +1,8 @@
-<script>
-</script>
-
 Hello, World!
+
+Наша команда состоит из двух участниковЖ
+
+Ткачёв Захар Алексеевич (<a href="https://github.com/bisspector">bisspector</a>)
+Злобинцев Илья Евгеньевич (<a href="https://github.com/ilyazzz">ilyazzz</a>)
+
+Нашей мотивацией является проявить, применить и потренировать свои навыки в реальных условиях.
